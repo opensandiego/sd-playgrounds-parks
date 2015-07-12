c03380ae250756b9b41ca300ec7c801ab9d5c75a
Glenn Batuyong <glenn@47ronin.com>
2015-07-12 12:54:33 -0700

    Initialize OpenStreetMap tileset

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,20 @@
  * Controller of the sdPlaygroundsParksApp
  */
 angular.module('sdPlaygroundsParksApp')
-  .controller('MainCtrl', function () {
-    
+  .controller('MainCtrl', function ($scope) {
+    angular.extend($scope, {
+      defaults: {
+        tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 18,
+        path: {
+          weight: 10,
+          color: '#800000',
+          opacity: 1
+        }
+      }, // defaults
+      center: {
+
+      } // center
+    })
   });
