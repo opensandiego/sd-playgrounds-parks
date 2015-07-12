08d0146b06b8a8991c1b4985df1cd8903f50b34d
Glenn Batuyong <glenn@47ronin.com>
2015-07-12 12:47:09 -0700

    Initial cleanup

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,5 @@
  */
 angular.module('sdPlaygroundsParksApp')
   .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    
   });
