9e563cf220dffe67490c93fec0c6e6b1ec889ba8
Glenn Batuyong <glenn@47ronin.com>
2015-07-12 12:45:03 -0700

    Angular Leaflet Directive dependency

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -33,6 +33,9 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/leaflet/dist/leaflet.js',
+      'bower_components/leaflet/dist/leaflet-src.js',
+      'bower_components/angular-leaflet-directive/dist/angular-leaflet-directive.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
