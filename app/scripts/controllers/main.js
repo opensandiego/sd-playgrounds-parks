6376b06a53e88cb198ce4e1555e6f1fbb87d3a31
enigma2522 <villan31@gmail.com>
2015-08-18 14:52:28 -0700

    Revert "lightweight"
    This reverts commit a10832aa8c299943137bea8f1f4a836abe75a80c.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sdPlaygroundsParksApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sdPlaygroundsParksApp
+ */
+angular.module('sdPlaygroundsParksApp')
+  .controller('MainCtrl', function ($scope) {
+    angular.extend($scope, {
+      defaults: {
+        tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 18,
+        path: {
+          weight: 10,
+          color: '#800000',
+          opacity: 1
+        }
+      }, // defaults
+      center: {
+        autoDiscover: true,
+        zoom: 18
+      } // center
+    })
+  });
