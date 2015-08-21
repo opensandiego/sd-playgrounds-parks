5adf19849fb1316ffb380c1b63dcd19bc3b87ed4
enigma2522 <villan31@gmail.com>
2015-08-20 19:35:47 -0700

    All references are external
    Made sure all references to libraries and library plugins are now
    external. New users will not need to download anything to their machine
    other than our source.

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -1,32 +1,36 @@
 function init() {
 
-//Fields to Display
-var displayableFields = ["ADDRESS_LO","OWNERNAME","OWNERTYPE","PARK_TYPE","DESIGNATED","COMMUNITY_","COUNCIL_DI"];
+    //Fields to Display
+    var displayableFields = ["ADDRESS_LO", "OWNERNAME", "OWNERTYPE", "PARK_TYPE", "DESIGNATED", "COMMUNITY_", "COUNCIL_DI"];
 
-//Field Aliases    
-var aliases = {
-"ADDRESS_LO":"Address",
-"OWNERNAME":"Owner",
-"OWNERTYPE":"Owner Type",
-"PARK_TYPE": "Type",
-"COMMUNITY_": "Community",
-"COUNCIL_DI": "Council District",
-"DESIGNATED": "Type"
-};
-    
+    //Field Aliases    
+    var aliases = {
+        "ADDRESS_LO": "Address",
+        "OWNERNAME": "Owner",
+        "OWNERTYPE": "Owner Type",
+        "PARK_TYPE": "Type",
+        "COMMUNITY_": "Community",
+        "COUNCIL_DI": "Council District",
+        "DESIGNATED": "Type"
+    };
+
     //create map
     var map = L.map('map').setView([33.0, -117.0], 10);
 
-    //ESRI imagery Layers - seemed slow on mobile devices
-    /*var mapLink = '<a href="http://www.esri.com/">Esri</a>';
-    var wholink = 'i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS'
-    
-    var esriWorld = L.tileLayer(
-        'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-            attribution: '&copy; ' + mapLink + ', ' + wholink,
-            maxZoom: 18,
-        }).addTo(map);*/
+    //Map Quest Aerial Imagery
+    var MapQuestOpen_Aerial = L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/{type}/{z}/{x}/{y}.{ext}', {
+        type: 'sat',
+        ext: 'jpg',
+        attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency',
+        subdomains: '1234'
+    });
 
+    //Dark Map Background - as per suggestion by meeting attendee
+    var CartoDB_DarkMatter = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+        subdomains: 'abcd',
+        maxZoom: 19
+    });
 
     // create the tile layer with correct attribution
     var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
@@ -35,112 +39,115 @@ var aliases = {
         attribution: osmAttrib
     }).addTo(map);
 
-    //Parks Geo json layer - built from shapefiles from http://www.sandag.org/index.asp?subclassid=100&fuseaction=home.subclasshome
-    //Shapefiles reprojection from state plane to WGS 84
-    //Convert from shapefile to GeoJSON by http://mapshaper.org/
-    //then combined in one file
-    var parksLayer = new L.GeoJSON.AJAX("./data/combined.json", {
-        onEachFeature: popUp
-    }).addTo(map);
-    
+    //feature normal style
+    var defaultStyle = {
+        color: "#006600",
+        weight: 2,
+        opacity: 0.6,
+        fillOpacity: 0.1,
+        fillColor: "#66ff66",
+        zIndex: 5
+    };
+
+    //feature highlighed style
+    var highlightStyle = {
+        color: '#006600',
+        weight: 2,
+        opacity: 0.6,
+        fillOpacity: 0.1,
+        fillColor: '#FFFF00',
+        zIndex: 5000
+    };
+
     //Allows you to attach events to features in layer
     function popUp(f, l) {
-        
-        //Output String start - header
-        var out = "<table><thead><th>Name</th><th>Value</th></thead>";
-        
-        //Put a common name attribute in each feature - makes search possible
-        if (f.properties.COMMON_NAM) {
-            f.properties.name = f.properties.COMMON_NAM;
-        } else if (f.properties.NAME_FULL) {
-            f.properties.name = f.properties.NAME_FULL;
-        } else if (f.properties.PARKNAME) {
-            f.properties.name = f.properties.PARKNAME;
-        } else {
-            f.properties.name = "Unknown";
-        }
 
-        //Name value for each feature
-        out += "<tr><td>Name</td><td>" + f.properties.name + "</td></tr>";
+            //Output String start - header
+            var out = "<table><thead><th>Name</th><th>Value</th></thead>";
 
-        if (f.properties) {
-            for (key in f.properties) {
-                for(propt in displayableFields)
-                {
-                 if (displayableFields[propt] == key) {
-                     
-                    if(aliases[key])
-                    {
-                    out += "<tr><td>" + aliases[key] + "</td><td>" + f.properties[key] + "</td></tr>";
-                    }
-                     else
-                     {
-                         out += "<tr><td>" + key + "</td><td>" + f.properties[key] + "</td></tr>";
-                     }
-                 }
-                }
+            //Put a common name attribute in each feature - makes search possible
+            if (f.properties.COMMON_NAM) {
+                f.properties.name = f.properties.COMMON_NAM;
+            } else if (f.properties.NAME_FULL) {
+                f.properties.name = f.properties.NAME_FULL;
+            } else if (f.properties.PARKNAME) {
+                f.properties.name = f.properties.PARKNAME;
+            } else {
+                f.properties.name = "Unknown";
             }
-            out += "</table>";
-            
-            //bind on click popup using output string
-            var popup = l.bindPopup(out);
-            
-            //label binging on mouse over - didn't see useful for mouse over on mobile
-            //had issues getting riseOnHover to work and also keeping them on the screen permanently
-            //l.bindLabel(f.properties["name"]);
 
-            //feature normal style
-            var defaultStyle = {
-                color: "#006600",
-                weight: 2,
-                opacity: 0.6,
-                fillOpacity: 0.1,
-                fillColor: "#66ff66",
-                zIndex: 5
-            };
-            
-            //feature highlighed style
-            var highlightStyle = {
-                color: '#006600',
-                weight: 2,
-                opacity: 0.6,
-                fillOpacity: 0.1,
-                fillColor: '#FFFF00',
-                zIndex: 5000
-            };
+            //Name value for each feature
+            out += "<tr><td>Name</td><td>" + f.properties.name + "</td></tr>";
 
-            //set the default style of the feature
-            l.setStyle(defaultStyle);
+            //biuld popup message
+            if (f.properties) {
+                for (key in f.properties) {
+                    for (propt in displayableFields) {
+                        if (displayableFields[propt] == key) {
 
-            l.on("mouseover", function (e) {
-                //set feature to highlight style on mouse over
-                l.setStyle(highlightStyle);
-                
-                //open popup on mouse over - not in use
-                //l.openPopup();
-            });
+                            if (aliases[key]) {
+                                out += "<tr><td>" + aliases[key] + "</td><td>" + f.properties[key] + "</td></tr>";
+                            } else {
+                                out += "<tr><td>" + key + "</td><td>" + f.properties[key] + "</td></tr>";
+                            }
+                        }
+                    }
+                }
+                out += "</table>";
 
-            l.on("mouseout", function (e) {
-                
-                //change feature to default style on mouse out
-                l.setStyle(defaultStyle);
-                
-                //close popup on mouse out - not in use
-                //l.closePopup();
+                //bind on click popup using output string
+                var popup = l.bindPopup(out);
+
+                l.on("mouseover", function (e) {
+                    //set feature to highlight style on mouse over
+                    l.setStyle(highlightStyle);
+
+                    //open popup on mouse over - not in use
+                    //l.openPopup();
+                });
+
+                l.on("mouseout", function (e) {
+
+                    //change feature to default style on mouse out
+                    l.setStyle(defaultStyle);
+
+                    //close popup on mouse out - not in use
+                    //l.closePopup();
+                });
+            }
+        }
+        //Parks Geo json layer - built from shapefiles from http://www.sandag.org/index.asp?subclassid=100&fuseaction=home.subclasshome
+        //Shapefiles reprojection from state plane to WGS 84
+        //Convert from shapefile to GeoJSON by http://mapshaper.org/
+        //then combined in one file
+    var parksLayer = new L.geoJson(null, {
+        style: function (feature) {
+            return defaultStyle;
+        },
+        onEachFeature: popUp
+    }).addTo(map);
+
+
+    $.ajax({
+        dataType: "json",
+        url: "data/combined.json",
+        success: function (data) {
+            $(data.features).each(function (key, data) {
+                parksLayer.addData(data);
             });
         }
-    }
+    }).error(function () {});
 
     //group basemaps for layer control use
     var basemap = {
-        //"World imagery": esriWorld,
+        "MapQuest Open Aerial": MapQuestOpen_Aerial,
+        "CartoDB Dark Matter": CartoDB_DarkMatter,
         "OpenStreetMap": osm
     }
 
     //group parks for layer control use - combine method
     var parks = {
-        
-        "Parks": parksLayer
+        "Park Areas": parksLayer
     };
 
 
@@ -161,7 +168,7 @@ var aliases = {
         setView: true,
         maxZoom: 15
     }).on("locationfound", function (e) {
-        console.log(e.latlng);
+        L.marker(e.latlng).bindLabel("You are here.",{noHide: true}).addTo(map);
     }, this);
 
 };
